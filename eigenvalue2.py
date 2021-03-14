23326d728e5f5761d3e996ce413a440df124ec56
EBaltas <78692621+EBaltas@users.noreply.github.com>
2021-03-14 22:31:11 +0000

    Included source polarisation, dof and 95% confidence region

--- eigenvalue2.py
+++ eigenvalue2.py
@@ -8,7 +8,10 @@ Created on Mon Mar  1 21:52:19 2021
 
 import numpy as np
 import obspy as obs
+from obspy.signal.util import next_pow_2
 import itertools
+from scipy.stats import f
+import matplotlib.pyplot as plt
 
 def RotMat2D(phi):
     
@@ -19,6 +22,10 @@ def RotMat2D(phi):
 def SourcePol(phicalc, V1_x, V1_y):
     
     return phicalc + np.rad2deg(np.arctan2(V1_x, V1_y)) + 180
+
+def CR95(L2, NDF, C=2, a=0.05):
+    
+    return L2*(1+((C/(NDF-C))*f.isf(a, C, NDF-C)))
 
 
 def SAndC_eigen(st_filt, starttime, endtime, timeDelay=0.25):
@@ -34,8 +41,6 @@ def SAndC_eigen(st_filt, starttime, endtime, timeDelay=0.25):
     phiSamples = np.arange(-90, 90+step, step, dtype=int)
     dttSamples = np.arange(0, timeDelay+step, step, dtype=int)
     
-    #phiSamples = np.arange(15, 24+step, step, dtype=int)
-    
     phiNumSamples = phiSamples.size
     dttNumSamples = dttSamples.size
     
@@ -49,9 +54,9 @@ def SAndC_eigen(st_filt, starttime, endtime, timeDelay=0.25):
     for i in range(phiNumSamples):
         phi = phiSamples[i]
         
-        R1 = RotMat2D(phi)
+        R = RotMat2D(phi)
         
-        FS = np.dot(R1, EN)
+        FS = np.dot(R, EN)
         
         for j in range(dttNumSamples):
             dtt = dttSamples[j]
@@ -86,9 +91,49 @@ def SAndC_eigen(st_filt, starttime, endtime, timeDelay=0.25):
     source_pol = SourcePol(phiCalc, V1[0], V1[1])
     
     print("source polarisation =", source_pol)
-    
 
-    return phiCalc, dttCalc/sps
+    
+    #Calculate rotatation matrix and delay window using phiCalc and dtt
+    R2 = RotMat2D(phiCalc)
+    delayWin2 = selectionWin + int(dttCalc)
+    
+    
+    #Rotate orginal EN using phiCalc
+    FS2 = np.dot(R2, EN)
+    
+    #Apply dtt
+    FS2_Lag = np.array((
+        FS2[0][delayWin2[0]:delayWin2[1]],
+        FS2[1][selectionWin[0]:selectionWin[1]]
+        ))
+   
+    #Rotate Fast-slow by source_pol - phiCalc
+    RSP = RotMat2D(source_pol - phiCalc)
+    
+    FS_SP = np.dot(RSP, FS2_Lag)
+    
+    #Calculating number of degrees of freedom
+    npoints = FS_SP[0].size
+    N2 = next_pow_2(npoints)
+    
+    spec_mag = abs(np.fft.fft(FS_SP[0], N2))
+    
+    F2 = (spec_mag**2).sum()-((spec_mag[0]**2)-(spec_mag[-1]**2))/2
+    F4 = (4/3)*(spec_mag**4).sum()-((spec_mag[0]**4)-(spec_mag[-1]**4))/3
+    
+    NDF = int(round((2*(F2**2)/F4)-1))
+    
+    print("Number of degrees of freedom =", NDF)
+    
+    L2 = Lambda2.copy()
+    
+    conf95 = CR95(L2.min(), NDF)
+    
+    L2_norm = L2/conf95
+    
+    cs = plt.contour(dttSamples/100, phiSamples, L2_norm)
+
+    return phiCalc, dttCalc/sps, FS2[0], FS2[1], FS2_Lag[0], FS2_Lag[1]
     
     
     
